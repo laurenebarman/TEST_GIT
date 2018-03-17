a094089a31a6d15b2cc7c216431a9f960bcd7724
Quentin <Quentin@10.91.129.165>
2018-03-17 12:20:30 +0100

    Indentation

--- src/TEST/Test.java
+++ src/TEST/Test.java
@@ -2,24 +2,24 @@ package TEST;
 
 public class Test {
 
-	
-		private String gitFromScrach;
-			
-			public Test (String parametre) {
-				this.gitFromScrach= parametre;
-				
-				
-				
-				String hello;
-				
-				
-				
-				
-			}
-			
-		}
-		
-		
-	
+
+	private String gitFromScrach;
+
+	public Test (String parametre) {
+		this.gitFromScrach= parametre;
+
+
+
+		String hello;
+
+
+
+
+	}
+
+}
+
+
+
 
 
